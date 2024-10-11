355197841e815636ccc27af2acfc52784b3f1f52
jeremymordecai628 <125563448+jeremymordecai628@users.noreply.github.com>
2024-10-11 12:21:09 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -9,9 +9,3 @@ def concat(str1: str, str2: str) -> str:
     """
     return str1 + str2
 
-if __name__ == "__main__":
-    str1 = "egg"
-    str2 = "shell"
-
-    print(concat(str1, str2) == "{}{}".format(str1, str2))
-    print(concat.__annotations__)
